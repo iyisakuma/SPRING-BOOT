c86fa1c70f9f3152d08f2adf9bef5e53f9e9f508
iyisakuma <yujisakuma224@gmail.com>
2023-05-19 17:04:36 -0300

    criado operação de listagem para medico

--- api/src/main/java/med/voll/api/controller/MedicoController.java
+++ api/src/main/java/med/voll/api/controller/MedicoController.java
@@ -5,11 +5,13 @@ import med.voll.api.medico.Medico;
 import med.voll.api.medico.MedicoDTO;
 import med.voll.api.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/medico")
@@ -17,9 +19,17 @@ public class MedicoController {
 
     @Autowired
     private MedicoRepository repository;
+
     @PostMapping
     @Transactional
-    public void cadastrar(@RequestBody @Valid MedicoDTO dto){
+    public void cadastrar(@RequestBody @Valid MedicoDTO dto) {
         repository.save(new Medico(dto));
+    }
+
+
+    @GetMapping
+    public Page<DadosMedicoListagem> listarMedicos(@PageableDefault(size = 10, sort = {"nome"}) Pageable page) {
+        return repository.findAll(page).map(DadosMedicoListagem::new);
+
     }
 }
